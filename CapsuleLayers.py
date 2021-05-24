Bug fix: Quantization of b parameter in Capsule Layer and tweak of softmax function to fit the implementatiuon of CMSIS-NN

CapsuleLayers.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import layers, initializers
 import tensorflow.keras.backend as k
 import tensorflow as tf
+import math
 
 
 class PrimaryCapsule(layers.Layer):
@@ -64,8 +65,9 @@ class Capsule(layers.Layer):
         self.output_ns_list = [None] * routings
         self.output_s_list = [None] * routings
         self.cc_list = [None] * routings
-        self.b_out_list = [None] * (routings-1)
-        self.b_bias_list = [None] * (routings-1)
+        self.b_inst_list = [None] * (routings-1)
+        self.b_old_list = [None] * (routings-1)
+        self.b_new_list = [None] * (routings-1)
 
     def build(self, input_shape):
         assert len(input_shape) >= 3, "The input Tensor should have shape=[None, input_num_capsule, input_dim_capsule]"
@@ -105,7 +107,8 @@ class Capsule(layers.Layer):
         assert self.routings > 0, 'The routings should be > 0.'
         for i in range(self.routings):
             # c.shape=[batch_size, num_capsule, input_num_capsule]
-            c = tf.nn.softmax(logits=b, axis=1)
+            c = tf.math.scalar_mul(1.0/math.log(2.0), b)
+            c = tf.nn.softmax(logits=c, axis=1)
             self.cc_list[i] = c
 
             # c_expanded.shape =  [None, num_capsule, 1, input_num_capsule]
@@ -130,9 +133,10 @@ class Capsule(layers.Layer):
                 output_expanded = k.expand_dims(x=output, axis=-1)
                 aux_b = tf.linalg.matmul(a=self.input_hat, b=output_expanded)
                 aux_b = tf.squeeze(input=aux_b, axis=-1)
-                self.b_bias_list[i] = b
+                self.b_inst_list[i] = aux_b
+                self.b_old_list[i] = b
                 b += aux_b
-                self.b_out_list[i] = b
+                self.b_new_list[i] = b
             # End: Routing algorithm -----------------------------------------------------------------------#
 
         return output
@@ -152,11 +156,14 @@ class Capsule(layers.Layer):
     def get_cc_list(self):
         return self.cc_list
 
-    def get_b_out_list(self):
-        return self.b_out_list
+    def get_b_inst_list(self):
+        return self.b_inst_list
 
-    def get_b_bias_list(self):
-        return self.b_bias_list
+    def get_b_new_list(self):
+        return self.b_new_list
+
+    def get_b_old_list(self):
+        return self.b_old_list
 
     def get_config(self):
         config = super().get_config()

Quantization_Backend.py
@@ -73,16 +73,18 @@ def get_cap_detailed_output(layer, input_data):
         output_ns_list_batch = layer.get_output_ns_list()
         output_s_list_batch = layer.get_output_s_list()
         cc_list_batch = layer.get_cc_list()
-        b_out_list_batch = layer.get_b_out_list()
-        b_bias_list_batch = layer.get_b_bias_list()
+        b_inst_list_batch = layer.get_b_inst_list()
+        b_new_list_batch = layer.get_b_new_list()
+        b_old_list_batch = layer.get_b_old_list()
 
         output_batch = output_batch.numpy()
         input_hat_batch = input_hat_batch.numpy()
         output_ns_list_batch = [x.numpy() for x in output_ns_list_batch]
         output_s_list_batch = [x.numpy() for x in output_s_list_batch]
         cc_list_batch = [x.numpy() for x in cc_list_batch]
-        b_out_list_batch = [x.numpy() for x in b_out_list_batch]
-        b_bias_list_batch = [x.numpy() for x in b_bias_list_batch]
+        b_inst_list_batch = [x.numpy() for x in b_inst_list_batch]
+        b_new_list_batch = [x.numpy() for x in b_new_list_batch]
+        b_old_list_batch = [x.numpy() for x in b_old_list_batch]
 
         if i > 0:
             output = np.append(output, output_batch, axis=0)
@@ -90,18 +92,20 @@ def get_cap_detailed_output(layer, input_data):
             output_ns_list = [np.append(x, y, axis=0) for x, y in zip(output_ns_list, output_ns_list_batch)]
             output_s_list = [np.append(x, y, axis=0) for x, y in zip(output_s_list, output_s_list_batch)]
             cc_list = [np.append(x, y, axis=0) for x, y in zip(cc_list, cc_list_batch)]
-            b_out_list = [np.append(x, y, axis=0) for x, y in zip(b_out_list, b_out_list_batch)]
-            b_bias_list = [np.append(x, y, axis=0) for x, y in zip(b_bias_list, b_bias_list_batch)]
+            b_inst_list = [np.append(x, y, axis=0) for x, y in zip(b_inst_list, b_inst_list_batch)]
+            b_new_list = [np.append(x, y, axis=0) for x, y in zip(b_new_list, b_new_list_batch)]
+            b_old_list = [np.append(x, y, axis=0) for x, y in zip(b_old_list, b_old_list_batch)]
         else:
             output = output_batch
             input_hat = input_hat_batch
             output_ns_list = output_ns_list_batch
             output_s_list = output_s_list_batch
             cc_list = cc_list_batch
-            b_out_list = b_out_list_batch
-            b_bias_list = b_bias_list_batch
+            b_inst_list = b_inst_list_batch
+            b_new_list = b_new_list_batch
+            b_old_list = b_old_list_batch
 
-    return output, input_hat, output_ns_list, output_s_list, cc_list, b_out_list, b_bias_list
+    return output, input_hat, output_ns_list, output_s_list, cc_list, b_inst_list, b_new_list, b_old_list
 
 
 def get_act_q_format_std_layer(io_layer):
@@ -146,13 +150,16 @@ def get_act_q_format_cap(io_layer):
         qmn_cc = {"int_bits": qm, "frac_bits": qn}
         fmt["cc_" + str(rout_it)] = qmn_cc
 
-    for rout_it, (b_out, b_bias) in enumerate(zip(io_layer['b_out_list'], io_layer['b_bias_list'])):
-        qm, qn = get_q_format(b_out)
-        qmn_b_out = {"int_bits": qm, "frac_bits": qn}
-        fmt["b_out_" + str(rout_it)] = qmn_b_out
-        qm, qn = get_q_format(b_bias)
-        qmn_b_bias = {"int_bits": qm, "frac_bits": qn}
-        fmt["b_bias_" + str(rout_it)] = qmn_b_bias
+    for rout_it, (b_inst, b_old, b_new) in enumerate(zip(io_layer['b_inst_list'], io_layer['b_old_list'], io_layer['b_new_list'])):
+        qm, qn = get_q_format(b_inst)
+        qmn_b_inst = {"int_bits": qm, "frac_bits": qn}
+        fmt["b_inst_" + str(rout_it)] = qmn_b_inst
+        qm, qn = get_q_format(b_old)
+        qmn_b_old = {"int_bits": qm, "frac_bits": qn}
+        fmt["b_old_" + str(rout_it)] = qmn_b_old
+        qm, qn = get_q_format(b_new)
+        qmn_b_new = {"int_bits": qm, "frac_bits": qn}
+        fmt["b_new_" + str(rout_it)] = qmn_b_new
 
     qm, qn = get_q_format(io_layer['output'])
     qmn_output = {"int_bits": qm, "frac_bits": qn}