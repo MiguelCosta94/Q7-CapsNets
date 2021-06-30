97e0dc0a683ef02cb143e069989a6f08b5049243
MiguelCosta94 <miguelcosta_mapc@live.com.pt>
2021-06-30 19:07:21 +0100

    Update quantization framework to use less RAM during quantization

--- Quantization.py
+++ Quantization.py
@@ -18,56 +18,89 @@ def load_mnist():
     dir = str(pathlib.Path(__file__).parent.absolute()) + "/../Datasets"
     train_db_dir = str(dir) + "/mnist_train.csv"
     test_db_dir = str(dir) + "/mnist_test.csv"
+    quant_db_dir = str(dir) + "/mnist_quantization.csv"
     train_db = pd.read_csv(train_db_dir, delimiter=',')
     test_db = pd.read_csv(test_db_dir, delimiter=',')
+    quant_db = pd.read_csv(quant_db_dir, delimiter=',')
 
     # Get features' values
     train_data = train_db.drop(columns=['label'])
     test_data = test_db.drop(columns=['label'])
+    quant_data = quant_db.drop(columns=['label'])
     train_data = train_data.values
     test_data = test_data.values
+    quant_data = quant_data.values
     scaler = preprocessing.MaxAbsScaler().fit(train_data)
     train_data = scaler.transform(train_data)
     test_data = scaler.transform(test_data)
+    quant_data = scaler.transform(quant_data)
     train_data = train_data.reshape(-1, 28, 28, 1).astype('float32')
     test_data = test_data.reshape(-1, 28, 28, 1).astype('float32')
+    quant_data = quant_data.reshape(-1, 28, 28, 1).astype('float32')
 
     # Get labels
     train_labels = train_db['label'].values
     test_labels = test_db['label'].values
+    quant_labels = quant_db['label'].values
     train_labels = to_categorical(train_labels.astype('float32'))
     test_labels = to_categorical(test_labels.astype('float32'))
+    quant_labels = to_categorical(quant_labels.astype('float32'))
 
-    return (train_data, train_labels), (test_data, test_labels)
+    return (train_data, train_labels), (test_data, test_labels), (quant_data, quant_labels)
 
 
-def get_io_per_layer(model, train_data):
-    output_per_layer = []
+def get_io_range_per_layer(model, quant_data):
+    range_per_layer = []
 
     for i, layer in enumerate(model.layers):
         if i == 0:
-            input = train_data
+            input = quant_data
         else:
             input = output
 
         if 'primary_capsule' in layer.get_config()['name']:
-            output, output_ns = qb.get_pcap_detailed_output(layer, input)
-            entry = {"layer": layer.get_config()["name"], "input": input, "output_ns": output_ns, "output": output}
-            output_per_layer.append(entry)
+            input_range, output_range, output_ns_range = qb.get_pcap_io_range(layer, input)
+            entry = {"layer": layer.get_config()["name"], "input": input_range, "output_ns": output_ns_range,
+                     "output": output_range}
+            range_per_layer.append(entry)
 
         elif 'capsule' in layer.get_config()['name']:
-            output, input_hat, output_ns_list, output_s_list, cc_list, b_inst_list, b_old_list, b_new_list = qb.get_cap_detailed_output(layer, input)
-            entry = {"layer": layer.get_config()["name"], "input": input, "input_hat": input_hat,
-                     "output_ns_list": output_ns_list, "output_s_list": output_s_list, "cc_list": cc_list,
-                     "b_inst_list": b_inst_list, "b_old_list": b_old_list, "b_new_list": b_new_list, "output": output}
-            output_per_layer.append(entry)
+            input_range, output_range, input_hat_range, output_ns_range, output_s_range, cc_range, b_inst_range,\
+                b_new_range, b_old_range = qb.get_cap_io_range(layer, input)
+
+            entry = {"layer": layer.get_config()["name"], "input": input_range, "input_hat": input_hat_range,
+                     "output_ns": output_ns_range, "output_s": output_s_range, "cc": cc_range,
+                     "b_inst": b_inst_range, "b_old": b_old_range, "b_new": b_new_range, "output": output_range}
+            range_per_layer.append(entry)
 
         else:
-            output = qb.get_layer_output(layer, input)
-            entry = {"layer": layer.get_config()["name"], "input": input, "output": output}
-            output_per_layer.append(entry)
+            input_range, output_range = qb.get_layer_io_range(layer, input)
+            entry = {"layer": layer.get_config()["name"], "input": input_range, "output": output_range}
+            range_per_layer.append(entry)
 
-    return output_per_layer
+        output = qb.get_layer_output(layer, input)
+
+    return range_per_layer
+
+
+def get_act_q_format(model, quant_data):
+    act_q_format = []
+    io_range_per_layer = get_io_range_per_layer(model, quant_data)
+
+    for io_range in io_range_per_layer:
+        if ('conv2d' in io_range['layer']) or ('dense' in io_range['layer']):
+            entry = qb.get_act_q_format_std_layer(io_range)
+            act_q_format.append(entry)
+
+        elif 'primary_capsule' in io_range['layer']:
+            entry = qb.get_act_q_format_pcap(io_range)
+            act_q_format.append(entry)
+
+        elif 'capsule' in io_range['layer']:
+            entry = qb.get_act_q_format_cap(io_range, model.get_layer(io_range["layer"]).routings)
+            act_q_format.append(entry)
+
+    return act_q_format
 
 
 def quantize_wt(model):
@@ -85,7 +118,9 @@ def quantize_wt(model):
                 wt = wt.transpose(3, 0, 1, 2)
 
             wt = wt.flatten()
-            qm, qn = qb.get_q_format(wt)
+            min_val = np.min(wt)
+            max_val = np.max(wt)
+            qm, qn = qb.get_q_format(min_val, max_val)
             wt_q_format = {"layer": layer.get_config()["name"], "int_bits": qm, "frac_bits": qn}
             wt_q_format_list.append(wt_q_format)
 
@@ -105,7 +140,9 @@ def quantize_bias(model):
         if len(wt) > 0:
             if len(wt) > 1:
                 bias = wt[1]        # Index 0 for weights, index 1 for bias
-                qm, qn = qb.get_q_format(bias)
+                min_val = np.min(bias)
+                max_val = np.max(bias)
+                qm, qn = qb.get_q_format(min_val, max_val)
                 bias_q = qb.quantize(bias, qn)
                 bias_q_format = {"layer": layer.get_config()["name"], "int_bits": qm, "frac_bits": qn}
                 bias_q_dict = {"layer": layer.get_config()["name"], "bias": bias_q}
@@ -122,30 +159,13 @@ def quantize_bias(model):
 
 
 def quantize_dataset(data):
-    qm, qn = qb.get_q_format(data)
+    min_val = np.min(data)
+    max_val = np.max(data)
+    qm, qn = qb.get_q_format(min_val, max_val)
     data = qb.quantize(data, qn)
     data = np.reshape(data, newshape=(np.shape(data)[0], -1))
 
     return data
-
-
-def get_act_q_format(io_per_layer):
-    act_q_format = []
-
-    for io_layer in io_per_layer:
-        if ('conv2d' in io_layer['layer']) or ('dense' in io_layer['layer']):
-            entry = qb.get_act_q_format_std_layer(io_layer)
-            act_q_format.append(entry)
-
-        elif 'primary_capsule' in io_layer['layer']:
-            entry = qb.get_act_q_format_pcap(io_layer)
-            act_q_format.append(entry)
-
-        elif 'capsule' in io_layer['layer']:
-            entry = qb.get_act_q_format_cap(io_layer)
-            act_q_format.append(entry)
-
-    return act_q_format
 
 
 def get_output_shift(act_q_format_list, wt_q_format_list, model):
@@ -266,20 +286,38 @@ def list_to_csv(data, filename):
                 fp.write(str(sample) + '\n\n')
 
 
+def get_layer_size(model):
+
+    print("WEIGHTS")
+    for layer in model.layers:
+        wt = layer.get_weights()
+        if len(wt) > 0:
+            wt = wt[0]        # Index 0 for weights, index 1 for bias
+            print("Layer: ", layer.get_config()['name'], " Size: ", sys.getsizeof(wt))
+
+    print("BIAS")
+    for layer in model.layers:
+        wt = layer.get_weights()
+        if len(wt) > 0:
+            if len(wt) > 1:
+                bias = wt[1]        # Index 0 for weights, index 1 for bias
+                print("Layer: ", layer.get_config()['name'], " Size: ", sys.getsizeof(bias))
+
+
+
 def main():
     start_time = time.time()
     model = tf.keras.models.load_model("caps_net.h5", custom_objects={'PrimaryCapsule': PrimaryCapsule,
                                                                       'Capsule': Capsule, 'Length': Length,
                                                                       'margin_loss': margin_loss})
 
-    (train_data, train_labels), (test_data, test_labels) = load_mnist()
-    output_per_layer = get_io_per_layer(model, test_data)
+    (train_data, train_labels), (test_data, test_labels), (quant_data, quant_labels) = load_mnist()
+    act_q_format_list = get_act_q_format(model, test_data)
     wt_q_list, wt_q_format_list = quantize_wt(model)
     bias_q_list, bias_q_format_list = quantize_bias(model)
-    test_data_q = quantize_dataset(test_data)
-    act_q_format_list = get_act_q_format(output_per_layer)
     output_shift_list = get_output_shift(act_q_format_list, wt_q_format_list, model)
     bias_shift_list = get_bias_shift(bias_q_format_list, act_q_format_list, wt_q_format_list, model)
+    test_data_q = quantize_dataset(test_data)
 
     dict_to_header_file(wt_q_list, "wt", "wt_q.h")
     dict_to_csv(wt_q_format_list, "wt_q_format.csv")
@@ -291,6 +329,8 @@ def main():
     dict_to_csv(bias_shift_list, "bias_shift.csv")
 
     print("Total time: ", time.time()-start_time)
+
+    get_layer_size(model)
 
 if __name__=='__main__':
     main()
--- Quantization_Backend.py
+++ Quantization_Backend.py
@@ -1,10 +1,7 @@
 import numpy as np
 
 
-def get_q_format(data):
-    # Quantize input data of the first layer
-    min_val = np.min(data)
-    max_val = np.max(data)
+def get_q_format(min_val, max_val):
     # Find number of integer bits to represent this range
     if (min_val == 0) and (max_val == 0):
         int_bits = 0
@@ -43,10 +40,26 @@ def get_layer_output(layer, input_data):
     return output
 
 
-def get_pcap_detailed_output(layer, input_data):
+def get_layer_io_range(layer, input_data):
     data = np.split(input_data, 1000)
-    output = []
-    output_ns = []
+    input_range = init_io_range()
+    output_range = init_io_range()
+
+    for data_batch in data:
+        output_batch = layer(data_batch)
+        output_batch = output_batch.numpy()
+
+        input_range = get_range_io(data_batch, input_range)
+        output_range = get_range_io(output_batch, output_range)
+
+    return input_range, output_range
+
+
+def get_pcap_io_range(layer, input_data):
+    data = np.split(input_data, 1000)
+    input_range = init_io_range()
+    output_range = init_io_range()
+    output_ns_range = init_io_range()
 
     for data_batch in data:
         output_batch = layer(data_batch)
@@ -54,20 +67,28 @@ def get_pcap_detailed_output(layer, input_data):
         output_batch_ns = layer.get_output_ns()
         output_batch_ns = output_batch_ns.numpy()
 
-        if len(output) > 0:
-            output = np.append(output, output_batch, axis=0)
-            output_ns = np.append(output_ns, output_batch_ns, axis=0)
-        else:
-            output = output_batch
-            output_ns = output_batch_ns
+        input_range = get_range_io(data_batch, input_range)
+        output_range = get_range_io(output_batch, output_range)
+        output_ns_range = get_range_io(output_batch_ns, output_ns_range)
 
-    return output, output_ns
+    return input_range, output_range, output_ns_range
 
 
-def get_cap_detailed_output(layer, input_data):
+def get_cap_io_range(layer, input_data):
     data = np.split(input_data, 1000)
+    num_rout = layer.routings
 
-    for i, data_batch in enumerate(data):
+    input_range = init_io_range()
+    output_range = init_io_range()
+    input_hat_range = init_io_range()
+    output_ns_range = init_io_range_routing(num_rout)
+    output_s_range = init_io_range_routing(num_rout)
+    cc_range = init_io_range_routing(num_rout)
+    b_inst_range = init_io_range_routing(num_rout-1)
+    b_new_range = init_io_range_routing(num_rout-1)
+    b_old_range = init_io_range_routing(num_rout-1)
+
+    for data_batch in data:
         output_batch = layer(data_batch)
         input_hat_batch = layer.get_input_hat()
         output_ns_list_batch = layer.get_output_ns_list()
@@ -86,32 +107,57 @@ def get_cap_detailed_output(layer, input_data):
         b_new_list_batch = [x.numpy() for x in b_new_list_batch]
         b_old_list_batch = [x.numpy() for x in b_old_list_batch]
 
-        if i > 0:
-            output = np.append(output, output_batch, axis=0)
-            input_hat = np.append(input_hat, input_hat_batch, axis=0)
-            output_ns_list = [np.append(x, y, axis=0) for x, y in zip(output_ns_list, output_ns_list_batch)]
-            output_s_list = [np.append(x, y, axis=0) for x, y in zip(output_s_list, output_s_list_batch)]
-            cc_list = [np.append(x, y, axis=0) for x, y in zip(cc_list, cc_list_batch)]
-            b_inst_list = [np.append(x, y, axis=0) for x, y in zip(b_inst_list, b_inst_list_batch)]
-            b_new_list = [np.append(x, y, axis=0) for x, y in zip(b_new_list, b_new_list_batch)]
-            b_old_list = [np.append(x, y, axis=0) for x, y in zip(b_old_list, b_old_list_batch)]
-        else:
-            output = output_batch
-            input_hat = input_hat_batch
-            output_ns_list = output_ns_list_batch
-            output_s_list = output_s_list_batch
-            cc_list = cc_list_batch
-            b_inst_list = b_inst_list_batch
-            b_new_list = b_new_list_batch
-            b_old_list = b_old_list_batch
+        input_range = get_range_io(data_batch, input_range)
+        output_range = get_range_io(output_batch, output_range)
+        input_hat_range = get_range_io(input_hat_batch, input_hat_range)
+        output_ns_range = get_range_io_routing(output_ns_list_batch, output_ns_range)
+        output_s_range = get_range_io_routing(output_s_list_batch, output_s_range)
+        cc_range = get_range_io_routing(cc_list_batch, cc_range)
+        b_inst_range = get_range_io_routing(b_inst_list_batch, b_inst_range)
+        b_new_range = get_range_io_routing(b_new_list_batch, b_new_range)
+        b_old_range = get_range_io_routing(b_old_list_batch, b_old_range)
 
-    return output, input_hat, output_ns_list, output_s_list, cc_list, b_inst_list, b_new_list, b_old_list
+    return input_range, output_range, input_hat_range, output_ns_range, output_s_range, cc_range, b_inst_range, b_new_range, b_old_range
+
+
+def init_io_range():
+    entry = {'min': 127, 'max': -128}
+    return entry
+
+
+def init_io_range_routing(num_rout):
+    entry = {}
+
+    for i in range(num_rout):
+        entry['min_'+str(i)] = 127
+        entry['max_'+str(i)] = -128
+
+    return entry
+
+
+def get_range_io(batch, range_io):
+    temp_min = np.min(batch)
+    temp_max = np.max(batch)
+    range_io['min'] = temp_min if (temp_min < range_io['min']) else range_io['min']
+    range_io['max'] = temp_max if (temp_max > range_io['max']) else range_io['max']
+
+    return range_io
+
+
+def get_range_io_routing(batch_list, range_io):
+    for i, batch in enumerate(batch_list):
+        temp_min = np.min(batch)
+        temp_max = np.max(batch)
+        range_io['min_' + str(i)] = temp_min if (temp_min < range_io['min_' + str(i)]) else range_io['min_' + str(i)]
+        range_io['max_' + str(i)] = temp_max if (temp_max > range_io['max_' + str(i)]) else range_io['max_' + str(i)]
+
+    return range_io
 
 
 def get_act_q_format_std_layer(io_layer):
-    qm, qn = get_q_format(io_layer["input"])
+    qm, qn = get_q_format(io_layer["input"]["min"], io_layer["input"]["max"])
     qmn_input = {"int_bits": qm, "frac_bits": qn}
-    qm, qn = get_q_format(io_layer["output"])
+    qm, qn = get_q_format(io_layer["output"]["min"], io_layer["output"]["max"])
     qmn_output = {"int_bits": qm, "frac_bits": qn}
     fmt = {"layer": io_layer['layer'], "input": qmn_input, "output": qmn_output}
 
@@ -119,49 +165,52 @@ def get_act_q_format_std_layer(io_layer):
 
 
 def get_act_q_format_pcap(io_layer):
-    qm, qn = get_q_format(io_layer["input"])
+    qm, qn = get_q_format(io_layer["input"]["min"], io_layer["input"]["max"])
     qmn_input = {"int_bits": qm, "frac_bits": qn}
-    qm, qn = get_q_format(io_layer['output'])
+    qm, qn = get_q_format(io_layer['output']["min"], io_layer['output']["max"])
     qmn_output = {"int_bits": qm, "frac_bits": qn}
-    qm, qn = get_q_format(io_layer['output_ns'])
+    qm, qn = get_q_format(io_layer['output_ns']["min"], io_layer['output_ns']["max"])
     qmn_output_ns = {"int_bits": qm, "frac_bits": qn}
     fmt = {"layer": io_layer['layer'], "input": qmn_input, "output_ns": qmn_output_ns, "output": qmn_output}
 
     return fmt
 
 
-def get_act_q_format_cap(io_layer):
-    qm, qn = get_q_format(io_layer["input"])
+def get_act_q_format_cap(io_layer, num_rout):
+    qm, qn = get_q_format(io_layer["input"]["min"], io_layer["input"]["max"])
     qmn_input = {"int_bits": qm, "frac_bits": qn}
-    qm, qn = get_q_format(io_layer['input_hat'])
+    qm, qn = get_q_format(io_layer['input_hat']["min"], io_layer['input_hat']["max"])
     qmn_input_hat = {"int_bits": qm, "frac_bits": qn}
     fmt = {"layer": io_layer['layer'], "input": qmn_input, "input_hat": qmn_input_hat}
 
     # Gets the quantized format of the output of every routing iteration
-    for rout_it, (output_ns, output_s, cc) in enumerate(zip(io_layer['output_ns_list'], io_layer['output_s_list'],
-                                                            io_layer['cc_list'])):
-        qm, qn = get_q_format(output_ns)
+    for i in range(num_rout):
+        qm, qn = get_q_format(io_layer["output_ns"]["min_"+str(i)], io_layer["output_ns"]["max_"+str(i)])
         qmn_output_ns = {"int_bits": qm, "frac_bits": qn}
-        fmt["output_ns_" + str(rout_it)] = qmn_output_ns
-        qm, qn = get_q_format(output_s)
+        fmt["output_ns_" + str(i)] = qmn_output_ns
+
+        qm, qn = get_q_format(io_layer["output_s"]["min_"+str(i)], io_layer["output_s"]["max_"+str(i)])
         qmn_output_s = {"int_bits": qm, "frac_bits": qn}
-        fmt["output_s_" + str(rout_it)] = qmn_output_s
-        qm, qn = get_q_format(cc)
+        fmt["output_s_" + str(i)] = qmn_output_s
+
+        qm, qn = get_q_format(io_layer["cc"]["min_"+str(i)], io_layer["cc"]["max_"+str(i)])
         qmn_cc = {"int_bits": qm, "frac_bits": qn}
-        fmt["cc_" + str(rout_it)] = qmn_cc
+        fmt["cc_" + str(i)] = qmn_cc
 
-    for rout_it, (b_inst, b_old, b_new) in enumerate(zip(io_layer['b_inst_list'], io_layer['b_old_list'], io_layer['b_new_list'])):
-        qm, qn = get_q_format(b_inst)
+    for i in range(num_rout-1):
+        qm, qn = get_q_format(io_layer["b_inst"]["min_"+str(i)], io_layer["b_inst"]["max_"+str(i)])
         qmn_b_inst = {"int_bits": qm, "frac_bits": qn}
-        fmt["b_inst_" + str(rout_it)] = qmn_b_inst
-        qm, qn = get_q_format(b_old)
-        qmn_b_old = {"int_bits": qm, "frac_bits": qn}
-        fmt["b_old_" + str(rout_it)] = qmn_b_old
-        qm, qn = get_q_format(b_new)
-        qmn_b_new = {"int_bits": qm, "frac_bits": qn}
-        fmt["b_new_" + str(rout_it)] = qmn_b_new
+        fmt["b_inst_" + str(i)] = qmn_b_inst
 
-    qm, qn = get_q_format(io_layer['output'])
+        qm, qn = get_q_format(io_layer["b_old"]["min_"+str(i)], io_layer["b_old"]["max_"+str(i)])
+        qmn_b_old = {"int_bits": qm, "frac_bits": qn}
+        fmt["b_old_" + str(i)] = qmn_b_old
+
+        qm, qn = get_q_format(io_layer["b_new"]["min_"+str(i)], io_layer["b_new"]["max_"+str(i)])
+        qmn_b_new = {"int_bits": qm, "frac_bits": qn}
+        fmt["b_new_" + str(i)] = qmn_b_new
+
+    qm, qn = get_q_format(io_layer['output']["min"], io_layer['output']["max"])
     qmn_output = {"int_bits": qm, "frac_bits": qn}
     fmt["output"] = qmn_output
 
